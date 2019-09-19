2fd1d2e7a0454f4cc8562813d7ae5468ee0e340c
Jorge Sandez <jorgesandez@ip-10-176-96-59.ec2.internal>
2019-09-19 18:19:27 -0300

    blok

--- Block.cpp
+++ Block.cpp
@@ -4,84 +4,85 @@
 
 // PRIVADO
 unsigned int Block::__getMax() {
-  unsigned int max = this->numbers[0];
-  for (int i = 0; i < this->size; i++) {
-    if (this->numbers[i] > max) {
-      max = this->numbers[i];
+    unsigned int max = this->numbers[0];
+    for (int i = 0; i < this->size; i++) {
+        if (this->numbers[i] > max) {
+            max = this->numbers[i];
+        }
     }
-  }
-  return max;
+    return max;
 }
 
 void Block::__setReference() {
-  this->reference = this->numbers[0];
-  for (int i = 1; i < this->size; i++) {
-    if (this->numbers[i] < reference) {
-      this->reference = this->numbers[i];
+    this->reference = this->numbers[0];
+    for (int i = 1; i < this->size; i++) {
+        if (this->numbers[i] < reference) {
+            this->reference = this->numbers[i];
+        }
     }
-  }
 }
 
 void Block::__setMaxBitSize() {
-  unsigned int max = __getMax();
-  unsigned int bits_amount = 1;
-  if (max == 0) {
-    this->bit_size = 0;
-  } else {
-    while (max > 1) {
-      max = max / 2;
-      bits_amount++;
+    unsigned int max = __getMax();
+    unsigned int bits_amount = 1;
+    if (max == 0) {
+        this->bit_size = 0;
+    } else {
+        while (max > 1) {
+            max = max / 2;
+            bits_amount++;
+        }
+        this->bit_size = bits_amount;
     }
-    this->bit_size = bits_amount;
-  }
 }
 
 // PUBLICO
 Block::Block(int size) {
-  this->size = size;
-  this->numbers = new unsigned int[size]();
+    this->size = size;
+    this->numbers = new unsigned int[size]();
 }
 
 void Block::fillNumbers(InputStream &input_stream) {
-  for (int i = 0; i < this->size; i++) {
-    unsigned int number = input_stream.getNumber();
-    if (number != -1) {
-      this->numbers[i] = number;
-    } else {
-      this->numbers[i] = this->numbers[i - 1];
+    for (int i = 0; i < this->size; i++) {
+        unsigned int number = input_stream.getNumber();
+        if (number != -1) {
+            this->numbers[i] = number;
+        } else {
+            this->numbers[i] = this->numbers[i - 1];
+        }
     }
-  }
 }
 
 void Block::resizeNumbers() {
-  for (int i = 0; i < this->size; i++) {
-    this->numbers[i] = this->numbers[i] - this->reference;
-  }
-  __setReference();
-  __setMaxBitSize();
+    __setReference();
+    for (int i = 0; i < this->size; i++) {
+        this->numbers[i] = this->numbers[i] - this->reference;
+    }
+    __setMaxBitSize();
 }
 
 void Block::fillList() {
-  for (int i = 0; i < this->size; i++) {
-    Bits bit_representation(this->bit_size);
-    bit_representation.represent(this->numbers[i]);
-    for (int j = this->bit_size - 1; j >= 0; j--) {
-      this->list_int.push_back(bit_representation.get(j));
+    for (int i = 0; i < this->size; i++) {
+        Bits bit_representation(this->bit_size);
+        bit_representation.represent(this->numbers[i]);
+        for (int j = this->bit_size - 1; j >= 0; j--) {
+            this->list_int.push_back(bit_representation.get(j));
+        }
     }
-  }
 }
 
 unsigned int Block::getReference() const {
-  return this->reference;
+    return this->reference;
 }
 
 unsigned int Block::getBitSize() const {
-  return this->bit_size;
+    return this->bit_size;
 }
 
 std::list<int> Block::getList() const {
-  return this->list_int;
+    return this->list_int;
 }
+
 Block::~Block() {
-  delete[] this->numbers;
+    delete[] this->numbers;
 }
